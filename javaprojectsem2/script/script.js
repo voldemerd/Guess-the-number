4d25f7b7875fd35273a2adc172f19bb80300c3b7
voldemerd <155643257+voldemerd@users.noreply.github.com>
2024-06-12 10:49:42 +0545

    Add files via upload

--- javaprojectsem2/script/script.js
+++ javaprojectsem2/script/script.js
@@ -4,8 +4,11 @@ let para2 = document.getElementById('tries');
 let button = document.getElementById('button');
 let para3 = document.getElementById('para3');
 let image = document.getElementById('imghide');
+let x =document.getElementById('para1');
+let y =document.getElementById('para2');
+let h1 =document.getElementById('head');
 let correctno = '';
-let j = 10;
+let j = 9;
 let array=[];
 window.onload = (event) => {
     correctno = parseInt(Math.random() * 101);
@@ -13,20 +16,31 @@ window.onload = (event) => {
 };
 
 button.addEventListener('click', function() {
+
     console.log(input.value);
     if (+input.value === correctno) {
         para3.textContent='Gome over';
         image.style.display='block';
+        button.style.display="none";
+        x.style.display="none";
+        y.style.display="none";
+        para1.style.display='none';
+        para2.style.display='none';
+        input.style.display='none';
+        para3.style.fontSize='50px';
+        h1.style.display='none';
         console.log('matched');
+        input.value="";
     } else {
+        input.value="";
         console.log('no');
         array.push(input.value);
         // console.log(array);
         para1.innerHTML = array;
-        if(j>=0)
+        if(j>0)
             {
                 para2.textContent=j--;
-                if(+input.value >= correctno)
+                if(+input.value > correctno)
                     {
                          para3.textContent='enter lower';
                     }
@@ -34,6 +48,18 @@ button.addEventListener('click', function() {
                 {
                     para3.textContent='enter higher';
                 }
+            }
+            else
+            {
+                button.style.display="none";
+                x.style.display="none";
+                y.style.display="none";
+                para1.style.display='none';
+                para2.style.display='none';
+                input.style.display='none';
+                para3.style.fontSize='50px';
+                para3.textContent='You lose';
+                h1.style.display='none';
             }
     }
 });
